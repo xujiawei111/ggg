56e27932369f5d969df6bf0a2459484c5b8c63c9
xujiawei <821562957@qq.com>
2020-12-25 23:10:47 +0800

    # WARNING: head commit changed in the meantime
    111

--- ui/TestGoods.py
+++ ui/TestGoods.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 from time import sleep
+
 from pymysql import connect
+from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-# 对不起，兄弟，你自己慢慢看吧
 class TestGoods():
-    
     def login(self):
-        driver = webdriver.Chrome("../chromedrive/chromedriver.exe")
+        driver = webdriver.Chrome()
         driver.get('http://192.168.1.4/ecshop/admin/privilege.php?act=login')
+
         driver.maximize_window()
         
         driver.find_element_by_name('username').send_keys('caichang')
@@ -126,4 +126,3 @@ class TestGoods():
         
         
         
-   
--- ui/demo.py
+++ ui/demo.py
@@ -1,7 +1,7 @@
 # -*-coding:UTF-8 -*-
 from selenium import webdriver
 
-driver = webdriver.Chrome("../chromedrive/chromedriver.exe")
+driver = webdriver.Chrome("..\chromedrive\chromedriver.exe")
 driver.get("http://192.168.1.4/ecshop/admin/privilege.php?act=login")
 driver.maximize_window()
 driver.find_element_by_name("username").send_keys("caichang")
